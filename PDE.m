3daa51757b5d500fc551fbebc116a0998ccf7c6b
Unknown <maartendoclo@hotmail.com>
2018-04-13 11:39:38 +0200

    Small comments

--- PDE.m
+++ PDE.m
@@ -9,7 +9,7 @@ U = poisson(f, N, north, east, south, west);
 
 h = 1/(N+1);
 x = h:h:N*h;
-UResult = zeros(N+2);
+UResult = zeros(N+2); %UResult bevat ook de randvoorwaarden
 UResult(2:N+1,2:N+1) = U;
 UResult(1,2:N+1) = arrayfun(south,x);
 UResult(N+2,2:N+1) = arrayfun(north,x);
@@ -18,16 +18,18 @@ UResult(2:N+1,N+2) = arrayfun(east,x);
 
 xx = 0:h:(N+1)*h;
 [XX,YY] = meshgrid(xx);
-figure()
+
+figure() %3d-oppervlak
 s = surf(XX,YY,UResult);
 hold on
-contour3(XX, YY, UResult, min(UResult):(max(UResult)-min(UResult))/17:max(UResult), 'LineWidth',1, 'Color','k')
+contour3(XX, YY, UResult, min(UResult):(max(UResult)-min(UResult))/17:max(UResult), 'LineWidth',1, 'Color','k') %contourlijnen op 3d-oppervlak
 hold off
 xlabel('x');
 ylabel('y');
 zlabel('u');
 s.EdgeColor = 'none';
-figure()
+
+figure()%2d-contour
 contour(XX,YY,UResult,min(UResult):(max(UResult)-min(UResult))/37:max(UResult),'ShowText','on','TextList',min(UResult):(max(UResult)-min(UResult))*4/37:max(UResult));
 xlabel('x');
 ylabel('y');
